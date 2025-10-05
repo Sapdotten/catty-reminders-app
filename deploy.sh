5ca21526d0de23a7f2ff92d678f697a73fa8de84
sapdotten <semenova.a.p.04@gmail.com>
2025-10-05 18:10:04 +0400

    chore: add deploy ans tests

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "🚀 Начинаем развертывание демо-сайта..."
+
+# Создаем директорию для сайта
+sudo mkdir -p /home/user/catty-reminders-app
+
+# Копируем файлы
+echo "📁 Копируем файлы сайта..."
+sudo cp . /home/user/catty-reminders-app
+source /home/user/catty-reminders-app/venv/bin/activate
+pip install -r /home/user/catty-reminders-app/requirements.txt
+
+sudo systemctl restart catty-reminders.service
---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "🧪 Running tests..."
+
+source /home/user/devops-demo-website/venv/bin/pip3 install -r ./requirements.txt && /home/user/devops-demo-website/venv/bin/python3 -m pytest
+pip install -r ./requirements.txt
+python3 -m pytest
