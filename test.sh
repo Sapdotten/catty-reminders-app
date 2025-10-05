4aa18e267aca94bf49c3e5f4d95a9cd99e616e90
sapdotten <semenova.a.p.04@gmail.com>
2025-10-05 18:18:37 +0400

    chore: add deploy ans tests

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 echo "🧪 Running tests..."
 
-source /home/user/devops-demo-website/venv/bin/pip3 install -r ./requirements.txt && /home/user/devops-demo-website/venv/bin/python3 -m pytest
+python3 -m venv venv
+source ./venv/activate/bin
 pip install -r ./requirements.txt
 python3 -m pytest
-        
