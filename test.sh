d981fcd36f963d6dbb2d118ffcffd5825680608e
sapdotten <semenova.a.p.04@gmail.com>
2025-10-05 18:14:42 +0400

    docs: update readme

--- test.sh
+++ test.sh
@@ -5,3 +5,4 @@ echo "🧪 Running tests..."
 source /home/user/devops-demo-website/venv/bin/pip3 install -r ./requirements.txt && /home/user/devops-demo-website/venv/bin/python3 -m pytest
 pip install -r ./requirements.txt
 python3 -m pytest
+        
